4570b6e1162f3d7c56393d52b1c534b44de7c372
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2024-09-09 13:44:33 -0400

    Add light-dark mode

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Header from "./Header.tsx";
 import Projects from "./Projects.tsx";
 import "../scss/App.scss";
@@ -8,27 +8,52 @@ import Contact from "./Contact.tsx";
 import Socials from "./Socials.tsx";
 
 function App() {
-    const ref = useRef(null);
+  const ref = useRef(null);
 
-    // Define breakpoints
-    const breakpoints = {
-        small: { min: 300, max: 499 },
-        medium: { min: 500, max: 899 },
-        large: { min: 900, max: 1399 },
-        extra: { min: 1400, max: Infinity },
-    };
+  // Define breakpoints
+  const breakpoints = {
+    small: { min: 300, max: 499 },
+    medium: { min: 500, max: 899 },
+    large: { min: 900, max: 1399 },
+    extra: { min: 1400, max: Infinity },
+  };
 
-    const size: string = useElementSize(ref, breakpoints) || "small";
+  const size: string = useElementSize(ref, breakpoints) || "small";
+  const videoRef = useRef(null);
+  const [videoSrc, setVideoSrc] = useState(
+    "https://videos.pexels.com/video-files/13936805/13936805-uhd_2560_1440_24fps.mp4",
+  );
 
-    return (
-        <div className={size} ref={ref}>
-            <Header />
-            <HomePage size={size}/>
-            <Projects size={size}/>
-            <Socials size={size}/>
-            <Contact size={size}/>
-        </div>
-    );
+  // Function to update the video source
+  const changeVideoSource = (newSrc: any) => {
+      setVideoSrc(newSrc);
+  };
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.load();
+      videoRef.current
+        .play()
+        .catch((error: any) =>
+          console.error("Error playing the video:", error),
+        );
+    }
+  }, [videoSrc]);
+
+  return (
+      <div className={size} ref={ref}>
+          <video id="background-video" ref={videoRef} autoPlay loop muted
+                 onError={(e) => console.log('Video Error:', e)}>
+              <source src={videoSrc} type="video/mp4"/>
+          </video>
+
+          <Header changeSource={changeVideoSource} size={size}/>
+          <HomePage size={size}/>
+          <Projects size={size}/>
+          <Socials size={size}/>
+          <Contact size={size}/>
+      </div>
+  );
 }
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,59 @@
 import React, { useState } from "react";
 import "../scss/Header.scss";
 
-const Header: React.FC = () => {
+// @ts-ignore
+interface HeaderProps {
+  changeSource?: (newSrc: any) => void;
+  size: any;
+}
+
+const Header: React.FC<HeaderProps> = ({ changeSource }, { size } ) => {
   const [isChecked, setIsChecked] = useState(true); // Default checked state
 
   const handleToggle = () => {
     setIsChecked(!isChecked);
     if (isChecked) {
-      document.documentElement.style.setProperty('--selected-background-color', 'var(--light-background-color)');
-      document.documentElement.style.setProperty('--selected-text-color', 'var(--light--text-color)');
-      document.documentElement.style.setProperty('--selected-secondary-color', 'var(--light-secondary-color)');
+      document.documentElement.style.setProperty(
+        "--selected-background-color",
+        "var(--light-background-color)",
+      );
+      document.documentElement.style.setProperty(
+        "--selected-text-color",
+        "var(--light--text-color)",
+      );
+      document.documentElement.style.setProperty(
+        "--selected-secondary-color",
+        "var(--light--secondary-color)",
+      );
+      document.documentElement.style.setProperty(
+          "--selected-button-color",
+          "var(--light--button-color)",
+      );
+      if (changeSource) {
+        changeSource("https://videos.pexels.com/video-files/6907162/6907162-hd_1080_1920_30fps.mp4");
+      }
     } else {
-      document.documentElement.style.setProperty('--selected-background-color', 'var(--background-color)');
-      document.documentElement.style.setProperty('--selected-text-color', 'var(--text-color)');
-      document.documentElement.style.setProperty('--selected-secondary-color', 'var(--secondary-color)');
+      document.documentElement.style.setProperty(
+        "--selected-background-color",
+        "var(--background-color)",
+      );
+      document.documentElement.style.setProperty(
+        "--selected-text-color",
+        "var(--text-color)",
+      );
+      document.documentElement.style.setProperty(
+        "--selected-secondary-color",
+        "var(--secondary-color)",
+      );
+      document.documentElement.style.setProperty(
+          "--selected-button-color",
+          "var(--button-color)",
+      );
+      if (changeSource) {
+        changeSource(
+            "https://videos.pexels.com/video-files/13936805/13936805-uhd_2560_1440_24fps.mp4",
+        );
+      }
     }
   };
 
@@ -49,10 +89,10 @@ const Header: React.FC = () => {
           <li className="header__items">
             <label className="header__items__toggle">
               <input
-                  checked={isChecked}
-                  onChange={handleToggle}
-                  type="checkbox"
-                  className="header__items__toggle__switch"
+                checked={isChecked}
+                onChange={handleToggle}
+                type="checkbox"
+                className="header__items__toggle__switch"
               />
               <span className="header__items__toggle__slider"></span>
             </label>
